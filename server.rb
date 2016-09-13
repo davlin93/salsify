3504ceb28d20ba5cc0fee8690e5786ea686c89a6
davlin93 <david@davidalin.com>
2016-09-13 14:50:29 -0400

    return 413 on out of bounds  - add proper newline to end of data file

--- server.rb
+++ server.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 
 get '/lines/:line_index'  do
+  line_index = params['line_index'].to_i
+
+  line_count = %x{wc -l < data.txt}.to_i
+
+  if line_index > (line_count - 1)
+    return status 413
+  end
+
+  puts "lines: #{line_count}"
   File.open("data.txt").each_with_index do |line, index|
-    if index == params['line_index'].to_i
+    if index == line_index
       return line
     end
   end
